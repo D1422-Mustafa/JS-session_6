ef321284f456a21d0c04625cac936f83f0792398
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-02-01 00:00:30 +0300

    6. session işlendi.

---
+++ Strings.js
@@ -0,0 +1,56 @@
+// let str1 = 'Hello'
+// let str2 = "World"
+// let str3 = `${str1 + str2}`
+
+// console.log(typeof str1, str1);
+// console.log(typeof str2, str2);
+// console.log(typeof str3, str3);
+
+// let str2= "World"
+// let str4 = new String("A string object")
+
+// // console.log(typeof str4, str4); //object
+
+// console.log(str4);
+// console.log(str2);
+
+//concat Methodu
+
+// var s1 = "Hello";
+// var s2 = "World!";
+// var s3 = s1.concat(s2);
+// console.log(s3);
+// console.log(s3.concat(' 2022 ', s1));
+// console.log({s1}, {s2});
+
+//charAt Methodu
+
+// var a = "primitive.\nlerin properti veya metodu olmaz."
+// console.log(a.charAt()); //boş girilirse ilk indexsi getirir.
+// console.log(a.charAt(9)); // bunun aynısı console.log(a[9]);
+// console.log(a.charAt(10));
+// console.log(a.charAt(11));
+// console.log(a.length);
+// console.log(a.charAt(43));
+// console.log(a.charAt(length-1));
+
+// console.log(a[a.length-1]);
+
+
+var str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+// //include Methodu
+
+// var n = str.includes("simply");
+// var n1 = str.includes("Simply");
+// console.log(n);
+// console.log(n1);
+
+// //indexOf
+// var n = str.indexOf("simply")
+// console.log(n);
+// console.log(str.indexOf("xa"));
+// console.log(str.indexOf("Simply"));
+
+// console.log(str.indexOf('m')); //ilk gördüğü sonucu döndürür.
+// console.log(str.lastIndexOf("m"));
